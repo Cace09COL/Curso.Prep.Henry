eadd3dfaa11ffb883a15369fa5ba20e2fb5cee81
Cace09COL <davidcamilobello@hotmail.com>
2022-11-14 15:02:37 -0500

    First commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -9,7 +9,7 @@ const nuevoNum = null;
 // Crea una variable booleana:
 const nuevoBool = null;
 
-// Resuelve el siguiente problema matemáticooooo:
+// Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - null === 5;
 
 // Resuelve el siguiente problema matemático:
